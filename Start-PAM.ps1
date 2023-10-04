38a277ebe9a9bad4ec9d6202cf93e43e1fe5e1f6
Jon Biggerstaff <83730440+jonbiggerstaff@users.noreply.github.com>
2023-10-03 20:34:20 -0700

    Added more checks and pauses
    Check for su target user. Add pause before kerberos refresh. Added more functions. Better comments

--- Start-PAM.ps1
+++ Start-PAM.ps1
@@ -1,22 +1,28 @@
-﻿#This script will add an account to privileged groups. It MUST be used in conjunction with the RemoveExpiredPrivileges.ps1 script in order for account to be removed from groups.
+﻿# 4368657374657273205765646765
+
+#This script will add an account to privileged groups. It MUST be used in conjunction with the RemoveExpiredPrivileges.ps1 script in order for account to be removed from groups.
 
 #Globals - Set these for your organization
     #Script title. Change when new version is released
-        $ScriptTitle = "==================== Welcome to Privileged Access Management 2.5 ===================="
+        $ScriptTitle = "==================== Welcome to Privileged Access Management 2.6 ===================="
     #Put your AD administrator account in the domain\username format. Example: abc\administrator
-        $domainAndUsername = ""
+        $domainAndUsername = "ms\administrator"
     #Put your domain in the "LDAP://DC=mydomain,DC=org" format. Example, for the AD domain in abc.xyz.org put "LDAP://DC=abc,DC=xyz,DC=org"
-        $CurrentDomain = ""
+        $CurrentDomain = "LDAP://DC=ms,DC=nsd,DC=org"
     #Send Mail From address
-        $SendMailFrom = ""
-    #Send Mail To Teams channel address. This is optional so comment or uncomment the Send-MailMessage lines in this script to enable or disable
-        $SendMailToChannel = ""
-    #Send Mail to email recipients. This is optional so comment or uncomment the Send-MailMessage lines in this script to enable or disable. Add commas for multiple addresses
-        $SendMailToRecipients = ""
+        $SendMailFrom = "loki@nsd.org"
+    #Send Mail To Teams channel address. Set next line to $true or $false
+        $ChannelAlertsEnabled = $true
+        $SendMailToChannel = "TempElevatedAccess - AC - Server Team <4ef1a885.nsd.org@amer.teams.ms>"
+    #Send Mail to email recipients. Set next line to $true or $false
+        $MailAlertsEnabled = $false
+        $SendMailToRecipients = "jbiggerstaff@nsd.org","ckacoroski@nsd.org"
     #SMTP server
-        $SMTPserver = ""
-    #UNV path for data files. IMPORTANT: Make sure the folder structure already exists. The script will create the files but not the folders. The account that runs the script will also need write access to this location
-        $PAMFiles = ""
+        $SMTPserver = "sys1.nsd.org"
+    #Location for data files. IMPORTANT: Make sure the folder structure already exists. The script will create the files but not the folders. The account that runs the script will also need write access to this location
+        $PAMFiles = "\\scheduler1\c$\nsd\script\PrivAccess"
+
+#Functions
 
 function Start-PAM {
 
@@ -27,54 +33,97 @@ function Start-PAM {
                 $adminusername = $cred.username
                 $adminpassword = $cred.GetNetworkCredential().password
                 $domain = New-Object System.DirectoryServices.DirectoryEntry($CurrentDomain,$adminUserName,$adminPassword)
-                if ($domain.name -eq $null) {
-                    Write-Host "Authentication failed."
+                    if ($domain.name -eq $null) {
+                        Write-Host "Authentication failed."
+                    }
                 }
-            }
-            while ($a -lt "3" -and $domain.name -eq $null)
+                while ($a -lt "3" -and $domain.name -eq $null)
 
-            if ($domain.name -eq $null)
-                {
-                write-host "Authentication failed - please verify the admin password."
-                break
-            } else {
-                $domainName = $domain.name
-                 write-host "Successfully authenticated with domain $domainName"
-            }
+                if ($domain.name -eq $null)
+                    {
+                    write-host "Authentication failed - please verify the admin password."
+                    break
+                } else {
+                    $domainName = $domain.name
+                     write-host "Successfully authenticated with domain $domainName"
+                }
     }
 
-    Get-Creds
+    function Confirm-TargetUser {
+                do {
+                $a = $a + 1
+                $global:username = [string]$(Read-Host 'Who are you granting this privilege to? (enter username-su)')
+                    if ($username -notlike "*-su") {
+                        Write-Host "Username does not end with -su."
+                    }
+                }
+                while ($a -lt "3" -and $username -notlike "*-su")
+
+                if ($username -notlike "*-su")
+                    {
+                    write-host "You failed too many times. Next time specify an su account."
+                    break
+                }
+    }
+
+    function Refresh-KerberosTicket {
+                if ($env:USERNAME -eq $username) {
+                    Start-Sleep -Seconds 3
+                    klist purge
+                    Write-Host -ForegroundColor Yellow "Your kerberos ticket has been refreshed"
+                }
+    }
 
     function Show-Menu {
         
-        Write-Host ""
-        Write-Host -ForegroundColor Yellow $ScriptTitle
-        Write-Host ""
-        Write-Host "1: AD Administrator    (Admin access to AD only)"
-        Write-Host "2: Domain Admin        (Admin access to AD and all domain joined systems)"
-        Write-Host "3: Schema Admin        (For making AD schema changes)"
-        Write-Host "q: Quit"
+            Write-Host ""
+            Write-Host -ForegroundColor Yellow $ScriptTitle
+            Write-Host ""
+            Write-Host "1: AD Administrator    (Admin access to AD only)"
+            Write-Host "2: Domain Admin        (Admin access to AD and all domain joined systems)"
+            Write-Host "3: Schema Admin        (For making AD schema changes)"
+            Write-Host "q: Quit"
+    }
+
+    function Send-ChannelAlert {
+                if ($ChannelAlertsEnabled -eq $true) {
+                    Send-MailMessage -From $SendMailFrom -To $SendMailToChannel -SmtpServer $SMTPserver -Subject "Privileged Access Notification" -Body $LogMsg
+                }
+    }
+
+    function Send-MailAlert {
+                if ($MailAlertsEnabled -eq $true) {
+                    Send-MailMessage -From $SendMailFrom -To $SendMailToRecipients -SmtpServer $SMTPserver -Subject "Privileged Access Notification" -Body $mailBody -BodyAsHtml
+                }
+    }
+
+    function Select-Option {
+                do {
+                $Selection = Read-Host "What level of privilege do you want to grant?"
+                    switch ($Selection)
+                    {
+                          '1' {
+                            $Global:Group = 'Administrators'
+                        } '2' {
+                            $Global:Group = 'Domain Admins'
+                        } '3' {
+                            $Global:Group = 'Schema Admins'
+                        } 'q' {
+                            return
+                        }
+            
+                    }
+                }
+                until (($Selection -eq "1") -or ($Selection -eq "2") -or ($Selection -eq "3") -or ($Selection -eq "q"))        
     }
  
+    Get-Creds   
     Show-Menu
 
 #Ask all the questions
-    $Selection = Read-Host "What level of privilege do you want to grant?"
-        switch ($Selection)
-         {
-               '1' {
-                 $Group = 'Administrators'
-             } '2' {
-                 $Group = 'Domain Admins'
-             } '3' {
-                 $Group = 'Schema Admins'
-             } 'q' {
-                 return
-             }
-         }
-
-    $username = [string]$(Read-Host 'Who are you granting this privilege to? (enter username-su)')
-    $expiration = $(Read-Host 'How many hours do they need it? (Example: 2 or .5)')
+    Select-Option
+    Confirm-TargetUser
+    $expiration = $(Read-Host 'How many hours do they need it? (Example: 1 or .5)')
     $reason = [string]$(Read-Host 'Why do they need it? (enter a brief explanation)')
 
 #Set the variables and add a PSDrive for accessing the csv and log file
@@ -113,10 +162,8 @@ function Start-PAM {
 ###Elevate user privileges
                 Add-ADGroupMember "$Group" $username -Credential $Cred
 #Refresh kerberos ticket if running on self
-                if ($env:USERNAME -eq $username) {
-                    klist purge
-                    Write-Host -ForegroundColor Yellow "Your kerberos ticket has been refreshed"
-                }
+                Refresh-KerberosTicket
+
                 Export-Csv -InputObject $csv -Path $csvlocation -NoTypeInformation -Append
                 Write-Host "The expiration time for $username has been updated to $expTime and is in the $Group group"
 #Log it and send notification to the Teams Server Team TempElevatedAccess channel
@@ -132,10 +179,10 @@ function Start-PAM {
                     $CurrentCSVContent
                     "
                 $LogMsg | Out-File $logLocation -Append
-#To disable Teams channel alerts, comment the following line
-                Send-MailMessage -From $SendMailFrom -To $SendMailToChannel -SmtpServer $SMTPserver -Subject "Privileged Access Notification" -Body $LogMsg
-#To disable email alerts, comment the following line
-                #Send-MailMessage -From $SendMailFrom -To $SendMailToRecipients -SmtpServer $SMTPserver -Subject "Privileged Access Notification" -Body $mailBody -BodyAsHtml
+
+#Send alerts if enabled
+                Send-ChannelAlert
+                Send-MailAlert
 
 #Cleanup
                 Remove-PSDrive dest
@@ -146,7 +193,7 @@ function Start-PAM {
                 Remove-PSDrive dest 
                 }
 
-    } else {
+            } else {
                 $ErrorActionPreference = 'silentlycontinue'
                 $userInAD = Get-ADUser $username
                 $ErrorActionPreference = 'continue'
@@ -160,10 +207,8 @@ function Start-PAM {
 ###Elevate user privileges
                 Add-ADGroupMember "$Group" $username -Credential $cred
 #Refresh kerberos ticket if running on self
-                if ($env:USERNAME -eq $username) {
-                    klist purge
-                    Write-Host -ForegroundColor Yellow "Your kerberos ticket has been refreshed"
-                }
+                Refresh-KerberosTicket
+                
                 Export-Csv -InputObject $csv -Path $csvlocation -NoTypeInformation -Append
                 Write-Host "`n$username has been added to the $Group group and will be removed at $expTime"
 #Log it and send notification to the Teams Server Team TempElevatedAccess channel
@@ -179,12 +224,12 @@ function Start-PAM {
                     $CurrentCSVContent
                     "
                 $LogMsg | Out-File $logLocation -Append
-#To disable Teams channel alerts, comment the following line
-                Send-MailMessage -From $SendMailFrom -To $SendMailToChannel -SmtpServer $SMTPserver -Subject "Privileged Access Notification" -Body $LogMsg
-#To disable email alerts, comment the following line
-                #Send-MailMessage -From $SendMailFrom -To $SendMailToRecipients -SmtpServer $SMTPserver -Subject "Privileged Access Notification" -Body $mailBody -BodyAsHtml
+
+#Send alerts if enabled
+                Send-ChannelAlert
+                Send-MailAlert
                 
 #Cleanup
                 Remove-PSDrive dest
-        }
+            }
 }
